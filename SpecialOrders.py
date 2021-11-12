5080bdd93448e4c262bb55f5922564be15e0bb6c
ucddj <75850663+ucddj@users.noreply.github.com>
2021-11-12 21:57:43 +0100

    findSeed, added recursivity just in case ; still missing ramissing randomized elements

--- SpecialOrders.py
+++ SpecialOrders.py
@@ -1,25 +1,25 @@
 from CSRandom import CSRandomLite
 
-def checkSpecialOrders(seed, daysPlayed, currentAndcompletedSpecialOrders=None,gingerIsland=False, islandResort=False,sewingMachine=False,qiChallengeBoxInUse=False):
-    if not currentAndcompletedSpecialOrders:
-        currentAndcompletedSpecialOrders = set()
-    season = int(daysPlayed / 28) % 4
-    dayOfMonth = ((daysPlayed-1) % 28) + 1
-    print("season", season, "dayOfMonth", dayOfMonth)
-    repeatableSpecialOrders = {"QiChallenge2", "QiChallenge3", "QiChallenge4", "QiChallenge5", "QiChallenge6", "QiChallenge7", "QiChallenge8",\
-        "QiChallenge9", "QiChallenge10", "QiChallenge12"}
-    monthLongSpecialOrders = {"Pierre", "Lewis", "Evelyn", "Caroline", "QiChallenge2", "QiChallenge4"}
-    baseSpecialOrders = ["Willy", "Pam", "Pierre", "Robin", "Emily", "Demetrius", "Demetrius2", "Gus", "Lewis", "Wizard", "Clint", "Linus",\
+BASE_SPECIAL_ORDERS = ["Willy", "Pam", "Pierre", "Robin", "Emily", "Demetrius", "Demetrius2", "Gus", "Lewis", "Wizard", "Clint", "Linus",\
         "Evelyn", "Wizard2", "Robin2", "Gunther", "Caroline", "Willy2", "QiChallenge2", "QiChallenge3", "QiChallenge4", "QiChallenge5",\
         "QiChallenge6", "QiChallenge7", "QiChallenge8","QiChallenge9", "QiChallenge10", "QiChallenge12"]
-    specialOrdersTitles = { "Willy": "Juicy Bugs Wanted!", "Pam": "The Strong Stuff", "Pierre": "Pierre's Prime Produce", "Robin": "Robin's Project",\
+SPECIAL_ORDERS_TITLES = { "Willy": "Juicy Bugs Wanted!", "Pam": "The Strong Stuff", "Pierre": "Pierre's Prime Produce", "Robin": "Robin's Project",\
         "Emily": "Rock Rejuvenation", "Demetrius": "Aquatic Overpopulation", "Demetrius2": "Biome Balance", "Gus": "Gus' Famous Omelet",\
         "Lewis": "Crop Order", "Wizard": "A Curious Substance", "Clint": "Cave Patrol", "Linus": "Community Cleanup", "Evelyn": "Gifts for George",\
         "Wizard2": "Prismatic Jelly", "Robin2": "Robin's Resource Rush", "Gunther": "Fragments of the past", "Caroline": "Island Ingredients",\
         "Willy2": "Tropical Fish", "QiChallenge2": "Qi's Crop", "QiChallenge3": "Let's Play A Game", "QiChallenge4": "Four Precious Stones",\
         "QiChallenge5": "Qi's Hungry Challenge", "QiChallenge6": "Qi's Cuisine", "QiChallenge7": "Qi's Kindness", "QiChallenge8": "Extended Family",\
         "QiChallenge9": "Danger In The Deep", "QiChallenge10": "Skull Cavern Invasion", "QiChallenge12": "Qi's Prismatic Grange"}
-    specialOrders = [specialOrder for specialOrder in baseSpecialOrders 
+
+def checkSpecialOrders(seed, daysPlayed, currentAndcompletedSpecialOrders=None,gingerIsland=False, islandResort=False,sewingMachine=False,qiChallengeBoxInUse=False):
+    if not currentAndcompletedSpecialOrders:
+        currentAndcompletedSpecialOrders = set()
+    season = int(daysPlayed / 28) % 4
+    dayOfMonth = ((daysPlayed-1) % 28) + 1
+    repeatableSpecialOrders = {"QiChallenge2", "QiChallenge3", "QiChallenge4", "QiChallenge5", "QiChallenge6", "QiChallenge7", "QiChallenge8",\
+        "QiChallenge9", "QiChallenge10", "QiChallenge12"}
+    monthLongSpecialOrders = {"Pierre", "Lewis", "Evelyn", "Caroline", "QiChallenge2", "QiChallenge4"}
+    specialOrders = [specialOrder for specialOrder in BASE_SPECIAL_ORDERS 
         if (specialOrder not in (currentAndcompletedSpecialOrders - repeatableSpecialOrders) and
             (dayOfMonth <= 15 or specialOrder not in monthLongSpecialOrders) and
             (gingerIsland or specialOrder != "Caroline") and
@@ -41,12 +41,49 @@ def checkSpecialOrders(seed, daysPlayed, currentAndcompletedSpecialOrders=None,g
         newSpecialOrder = rand.Next(len(validList))
         result.append(validList[newSpecialOrder])
         rand.Next()
-    return [f"{specialOrdersTitles[res]} ({res})" for res in result]
+    return result
 
+def findSeed(startingDay, dayLimit, targetMission, currentAndCompleted=None, maxDepth=2, gingerIsland=False, islandResort=False,sewingMachine=False,qiChallengeBoxInUse=False):
+    def _rec_findSeed(_startingDay, path, depth):
+        currentDay = _startingDay
+        while currentDay <= dayLimit:
+            baseOrders = checkSpecialOrders(seed, currentDay, set(path), gingerIsland, islandResort,sewingMachine,qiChallengeBoxInUse)
+            if targetMission in baseOrders:
+                return [{"seed" : seed, "day": currentDay, "order" : path}]
+            if depth > 0:
+                orders1 = _rec_findSeed(currentDay + 7, path + [baseOrders[0]], depth - 1)
+                orders2 = _rec_findSeed(currentDay + 7, path + [baseOrders[1]], depth - 1)
+                if not orders1 and not orders2: 
+                    return
+                resDepth = []
+                if orders1:
+                    resDepth += orders1
+                if orders2:
+                    resDepth += orders2
+                return [{"seed":seed, "day": res["day"], "order" : res["order"]} for res in resDepth]
+            currentDay += 7
+        return
+
+    if dayLimit < startingDay:
+        return
+    if not currentAndCompleted:
+        currentAndCompleted = []
+    seed = 0
+    while 1:
+        if seed % 10000 == 1:
+            print(seed - 1)
+        seed_res = _rec_findSeed(startingDay, currentAndCompleted, maxDepth)
+        if seed_res:
+            print(seed_res)
+        seed += 1
 
 if __name__ == '__main__':
-    
+    print(findSeed(57, 57, "Caroline", gingerIsland=True))
 
+    #print(findSeed(57, 100, "Caroline", gingerIsland=True))
+
+
+def __test(checkSpecialOrders):
     print(checkSpecialOrders(295358305, 57, None, gingerIsland=True))
     print(checkSpecialOrders(315083800, 57, {}))
     print(checkSpecialOrders(315083800, 64, {"Wizard"}))
